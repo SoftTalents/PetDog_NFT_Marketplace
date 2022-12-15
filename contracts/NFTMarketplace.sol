b45232b68ccfdb78ad3d09fb585c95aeb5c1931b
cryptohero92 <narutoninja815@hotmail.com>
2022-12-15 11:41:08 +0900

    createToken, getAllTokens added to contract

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,2 +1,87 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+    address payable owner; // the owner of the smart contract
+    uint256 listPrice = 0.001 ether;
+
+    struct ListedToken {
+        uint256 tokenId;
+        address payable owner;
+        address payable seller;
+        uint256 price;
+        bool currentlylisted;
+    }
+
+    event TokenListedSuccess (
+        uint256 indexed tokenId,
+        address owner,
+        address seller,
+        uint256 price,
+        bool currentlyListed
+    );
+
+    mapping(uint256 => ListedToken) private idToListedToken;
+
+    constructor() ERC721("NFTMarketplace", "NFTM") {
+        owner = payable(msg.sender);
+    }
+
+    function createToken(string memory tokenURI, uint256 price) public payable returns (uint) {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+
+        // Mint the NFT with newTokenId to the address who called createToken
+        _safeMint(msg.sender, newTokenId);
+        
+        _setTokenURI(newTokenId, tokenURI);
+
+        createListedToken(newTokenId, price);
+        return newTokenId;
+    }
+
+    function createListedToken(uint256 tokenId, uint256 price) private {
+        require(msg.value == listPrice, "Hopefully sending the correct price");
+        require(price > 0, "Make sure the price isn't negative");
+
+        idToListedToken[tokenId] = ListedToken(
+            tokenId,
+            payable(address(this)),
+            payable(msg.sender),
+            price,
+            true
+        );
+
+        _transfer(msg.sender, address(this), tokenId);
+        emit TokenListedSuccess(
+            tokenId,
+            address(this),
+            msg.sender,
+            price,
+            true
+        );
+    }
+
+    // This will returns all the NFTs current listed to be sold on the marketplace.
+    function getAllNFTs() public view returns (ListedToken[] memory) {
+        uint nftCount = _tokenIds.current();
+        ListedToken[] memory tokens = new ListedToken[](nftCount);
+        uint currentIndex = 0;
+
+        for (uint i = 0; i < nftCount; i++) {
+            uint currentId = i + 1;
+            ListedToken storage currentItem = idToListedToken[currentId];
+            tokens[currentIndex] = currentItem;
+            currentIndex += 1;
+        }
+        return tokens;
+    }
+}
